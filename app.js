fbbf143529b1bcc51fd338015b258e70900548df
Cody Reed <cody2j2reed@gmail.com>
2018-12-27 18:11:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const request = require("request");
 
 const google = require("./api.js");
 
-console.log(google.default);
-
 request(
   {
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=2131%20woodrow%20drive%20knoxville&key=${
@@ -12,6 +10,6 @@ request(
     json: true
   },
   (error, response, body) => {
-    console.log(body);
+    console.log(JSON.stringify(body, undefined, 2));
   }
 );
