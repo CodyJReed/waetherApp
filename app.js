1e578060098b0698eaa7f1cf4b3be15328c5b6f2
Cody Reed <cody2j2reed@gmail.com>
2018-12-27 20:35:54 -0500

    Added yargs.js via yarn/Updated package.json/Updated app.js

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 const request = require("request");
+const yargs = require("yargs");
 
 const google = require("./api.js");
 
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: "address",
+      describe: "Address to fetch weather for.",
+      string: true
+    }
+  })
+  .help()
+  .alias("help", "h").argv;
+
 request(
   {
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=2131%20woodrow%20drive%20knoxville&key=${
-      google.default
-    }`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      argv.a
+    )}&key=${google.default}`,
     json: true
   },
   (error, response, body) => {
-    console.log(`Address: ${response.body.results[0].formatted_address}`);
+    if (error) {
+      console.log("Unable to connect to Google servers.");
+    } else if (body.status === "ZERO_RESULTS") {
+      console.log("Unable to find address.");
+    } else if (body.status === "OK") {
+      console.log(`Address: ${response.body.results[0].formatted_address}`);
 
-    console.log(`Longitude: ${response.body.results[0].geometry.location.lng}`);
+      console.log(
+        `Longitude: ${response.body.results[0].geometry.location.lng}`
+      );
 
-    console.log(`Latitude: ${response.body.results[0].geometry.location.lat}`);
+      console.log(
+        `Latitude: ${response.body.results[0].geometry.location.lat}`
+      );
+    }
   }
 );
