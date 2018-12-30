44ff89fdb29ace68e3a3e0465726c1b9231a6800
Cody Reed <cody2j2reed@gmail.com>
2018-12-30 08:44:39 -0500

    Updated @params on getWeather

--- app.js
+++ app.js
@@ -15,7 +15,13 @@ const argv = yargs
   .help()
   .alias("help", "h").argv;
 
-getWeather();
+getWeather(36.0244067, -83.93834749999999, (errMsg, wthrRslt) => {
+  if (errMsg) {
+    console.log(errMsg);
+  } else {
+    console.log(JSON.stringify(wthrRslt, undefined, 2));
+  }
+});
 
 // geocodeAddress(argv.a, (errMsg, rslt) => {
 //   if (errMsg) {
--- forecast/forecast.js
+++ forecast/forecast.js
@@ -1,14 +1,14 @@
 const request = require("request");
 const { skyKey } = require("../api.js");
 
-const getWeather = () => {
+const getWeather = (lat, lng, cb) => {
   request(
     {
-      url: `https://api.darksky.net/forecast/${skyKey}/36.0244067,-83.93834749999999`,
+      url: `https://api.darksky.net/forecast/${skyKey}/${lat},${lng}`,
       json: true
     },
     (error, response, body) => {
-      console.log(
+      cb(
         error || response.statusCode !== 200
           ? "Unable to connect to forecast.io"
           : body.currently.temperature
