6bf304a1425122c26d866bbdb9ed04337b726bdb
Cody Reed <cody2j2reed@gmail.com>
2019-01-02 09:20:18 -0500

    Update promise.js
    Created asyncAdd function, returning new Promise.
    
    Called asyncAdd and worked on Promise chaining.

--- playground/promise.js
+++ playground/promise.js
@@ -1,15 +1,46 @@
-var somePromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    // resolve("We're figuring this out.");
-    reject("Uhoh...");
-  }, 2500);
-});
+var asyncAdd = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof a === "number" && typeof b === "number") {
+        resolve(a + b);
+      } else {
+        reject("Arguments must be numbers.");
+      }
+    }, 1500);
+  });
+};
 
-somePromise.then(
-  msg => {
-    console.log("Success:", msg);
-  },
-  errMsg => {
-    console.log("Error:", errMsg);
-  }
-);
+asyncAdd(5, 7)
+  .then(
+    res => {
+      console.log("Result:", res);
+      return asyncAdd(res, 33);
+    },
+    errMsg => {
+      console.log(errMsg);
+    }
+  )
+  .then(
+    res => {
+      console.log("Should be 45", res);
+    },
+    errMsg => {
+      console.log(errMsg);
+    }
+  );
+
+// var somePromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // resolve("We're figuring this out.");
+//     reject("Uhoh...");
+//   }, 2500);
+// });
+//
+// somePromise.then(
+//   msg => {
+//     console.log("Success:", msg);
+//   },
+//   errMsg => {
+//     console.log("Error:", errMsg);
+//   }
+// );
