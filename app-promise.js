6758f204166feac032473d294651253aa9213a9a
Cody Reed <cody2j2reed@gmail.com>
2019-01-04 10:17:44 -0500

    Update app-promise.js

--- app-promise.js
+++ app-promise.js
@@ -20,21 +20,27 @@ const encodedAddress = encodeURIComponent(argv.address);
 const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${mapsKey}`;
 
 axios
+  // GET request to geocodeUrl
   .get(geocodeUrl)
+  // Take response and check condition
   .then(res => {
+    // If 0 results
     if (res.data.status === "ZERO_RESULTS") {
       throw new Error("Unable to find address.");
     }
+    // If success
+    console.log(res.data.results[0].formatted_address);
 
     let lat = res.data.results[0].geometry.location.lat;
     let lng = res.data.results[0].geometry.location.lng;
     const forecastUrl = `https://api.darksky.net/forecast/${skyKey}/${lat},${lng}`;
-    console.log(res.data.results[0].formatted_address);
+    // Return new GET request
     return axios.get(forecastUrl);
   })
   .then(res => {
     let temperature = res.data.currently.temperature;
     let apparentTemperature = res.data.currently.apparentTemperature;
+
     console.log(
       `It's currently ${temperature}. It feels like ${apparentTemperature}`
     );
